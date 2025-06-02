11628ea5ec05acb01c1a2d896d22c034be103550
343330528 <343330528@C-J55TKB2.tdsb.on.ca>
2025-06-02 09:13:38 -0400

    First Commit

---
+++ src/ibongadarnagame/MySketch.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ibongadarnagame;
+
+import processing.core.PApplet;
+/**
+ *
+ * @author 343330528
+ */
+public class MySketch extends PApplet {
+    
+    public void settings() {
+        size(400, 400);
+    }
+    
+    public void setup() {
+        background(100, 100, 100);
+    }
+    
+    public void draw() {
+        
+    }
+}
