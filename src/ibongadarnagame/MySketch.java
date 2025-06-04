2edbebd330bbed8b3e37d05e551710a0eed7c0f2
343330528 <343330528@C-J55TKB2.tdsb.on.ca>
2025-06-04 10:19:52 -0400

    MySketch

--- src/ibongadarnagame/MySketch.java
+++ src/ibongadarnagame/MySketch.java
@@ -12,51 +12,82 @@ import processing.core.PImage;
 public class MySketch extends PApplet {
     //Variables
     int stage = 0; //Current stage of the game starts at 0
-    String userInput = ""; //Stores user text
+    String userInput = ""; //Stores user input text
     //Variables for Stage -1 (Start Game)
     boolean customName = false; //True if user enters their own name for the character
     boolean customCharacter = false; //True if user customizes clothing colors of the character
     //Variables for Stage 0 (Main Menu)
-     
-    //Load images for Stage 0 (Main Menu)
-    PImage title = loadImage("images/title.jpg");
-    PImage startGame = loadImage("images/startgame.jpg");
-    PImage leaderboard = loadImage("images/leaderboard.jpg");
-    PImage exitGame = loadImage("images/exitGame.jpg");
-    //Load images for Stage 1 (Start Game Screen)
-    PImage character = loadImage("images/player,jpg");
-    PImage leftButton = loadImage("images/leftbutton.jpg");
-    PImage rightButton = loadImage("images/rightbutton.jpg");
-    PImage customname = loadImage("images/customname.jpg");
-    PImage customcharacter = loadImage("images/customcharacter.jpg");
     
+    //Buttons
+    //Buttons for Stage 0 (Main Menu)
+    boolean overTestImg = false; ////Set variable indicating mouse is over button to false
+    int buttonIndex = 0; ////
+    int testImgX = 0; ////Set x position of button
+    int testImgY = 0; ////Set y position of button
+            
+     
+    //Declare images
+    //Declare images for Stage 0 (Main Menu)
+    PImage testimg; ////
+    PImage title;          //Title of story
+    PImage startGame;      //Start game button
+    PImage leaderboard;    //Leaderboard button
+    PImage exitGame;       //Exit game button
+    //Declare images for Stage 1 (Start Game Screen)
+    PImage character;      //Character
+    PImage leftButton;     //Left arrow button
+    PImage rightButton;    //Right arrow button
+    PImage customname;     //Custom name button
+    PImage customclothes;  //Custom clothes button
     
     public void settings() {
-        size(600, 500);
+        size(800, 600); //Set size of frame
     }
     
     public void setup() {
-        background(100, 100, 100); //Set background
+        background(100, 100, 100); ////Set background
         textSize(20); //Set text size
+        
+        //Load images
+        //Load images for Stage 0 (Main Menu)
+        testimg = loadImage("testimg.png"); ////
+        title = loadImage("images/title.jpg");                 //Title of story
+        startGame = loadImage("images/startgame.jpg");         //Start game button
+        leaderboard = loadImage("images/leaderboard.jpg");     //Leaderboard button
+        exitGame = loadImage("images/exitGame.jpg");           //Exit game button
+        //Load images for Stage 1 (Start Game Screen)
+        character = loadImage("images/player,jpg");            //Character
+        leftButton = loadImage("images/leftbutton.jpg");       //Left arrow button
+        rightButton = loadImage("images/rightbutton.jpg");     //Right arrow button
+        customname = loadImage("images/customname.jpg");       //Custom name button
+        customclothes = loadImage("images/customclothes.jpg"); //Custom clothes button
+        
     }
     
     public void draw() {
+        background(255); //Reset background
+        update(mouseX, mouseY); //Update variables indicating button clicks
+        
         //Main menu
         if (stage == 0) {
+            
             //Set background
-            fill(255, 0, 0);
+            
             //Display title
             image(title, 0, 0);
             //Display button options
             rect(200, 200, 200, 30);
-            image(startGame, 0, 0);   //Start game button
+            image(testimg, 0, 0); ////
+            
+            
+            image(startGame, testImgX, testImgY);   //Start game button
             image(leaderboard, 0, 0); //Show leaderboard button
             image(exitGame, 0, 0);    //Exit game button
             
         //Start game
         } else if (stage == -1) {
             image(customname, 200, 500); 
-            image(customcharacter, 200, 500); 
+            image(customclothes, 200, 500); 
             image(character, 200, 500); 
             
             if (customName) {
@@ -69,6 +100,7 @@ public class MySketch extends PApplet {
                 image(rightButton, 200, 500); 
             }
             
+            
         } else if (stage == -2) {
             image(character, 200, 500); 
             image(leftButton, 200, 500); 
@@ -78,7 +110,11 @@ public class MySketch extends PApplet {
             image(character, 200, 500); 
             image(leftButton, 200, 500); 
             image(rightButton, 200, 500); 
-        }
+            
+        } else if (stage == 1) {
+            fill(255, 255, 0);
+            image(testimg, 50, 0);
+        } 
     }
     
     
@@ -86,10 +122,24 @@ public class MySketch extends PApplet {
         ////unfinished
     }
     
+    void update(int x, int y) {
+        //set all booleans in array to false
+        
+        //If mouse is hovering over the the 
+        if (overImage(testimg, testImgX, testImgY))
+            overTestImg = true;
+        else
+            overTestImg = false;
+            
+    }
     
     @Override
     public void mousePressed() {
         if (stage == 0) {
+            if (overTestImg) {
+                stage = 1;
+            }////
+            /*
             if (overImage(startGame)) {
               stage = 1;
             }
@@ -106,23 +156,15 @@ public class MySketch extends PApplet {
             if (overImage(customcharacter)) {
               customCharacter = true;
             } 
+*////
         }
             
     }
     
-    
-    boolean overImage(int x, int y, int width, int height)  {
-        if (mouseX >= x && mouseX <= x+width && 
-            mouseY >= y && mouseY <= y+height) {
-          return true;
-        } else {
-          return false;
-        }
-    }
-    
-    boolean overImage(PImage image)  {
-        if (mouseX >= image.x && mouseX <= image.x+image.width && 
-            mouseY >= image.y && mouseY <= image.y+image.height) {
+     
+   boolean overImage(PImage image, int x, int y)  {
+        if (mouseX >= x && mouseX <= x+image.width && 
+            mouseY >= y && mouseY <= y+image.height) {
           return true;
         } else {
           return false;
