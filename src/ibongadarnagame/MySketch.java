be4b16c8141b679477dddbef9714b9c160c8504d
jones <jones@macbookair>
2025-06-05 22:54:49 -0400

    MySketch

--- src/ibongadarnagame/MySketch.java
+++ src/ibongadarnagame/MySketch.java
@@ -13,12 +13,17 @@ public class MySketch extends PApplet {
     //Variables
     int stage = 0; //Current stage of the game starts at 0
     String userInput = ""; //Stores user input text
+    String enteredName = ""; //Player's name (based on user input, by default set to "Don Juan")
     //Variables for Stage -1 (Start Game)
     boolean customName = false; //True if user enters their own name for the character
     boolean customCharacter = false; //True if user customizes clothing colors of the character
+    //Variables for Stage -2 (Choose Name)
+    boolean nameError = false; //True if name entered by user is not between 1 - 10 characters long (error)
     //Variables for Stage -3 (Customize Clothes)
     boolean chooseCharacter1 = true;  //True if character clothing v1 is currently displayed
     boolean chooseCharacter2 = false; //True if character clothing v2 is currently displayed
+    //Variables for Stage 1 (Begin Story)
+    int currentScreen1 = 1; //Keeps track of the different screens being shown in the same stage
     
     //Buttons
     //Buttons for Stage 0 (Main Menu)
@@ -37,22 +42,22 @@ public class MySketch extends PApplet {
     
     //Buttons for Stage -1 (Character Setup)
     boolean overChooseName = false; //Set variable indicating mouse is over button to false
-    int chooseNameX = 0; //Set x position of choose name button
-    int chooseNameY = 0; //Set y position of choose name button
+    int chooseNameX = 230; //Set x position of choose name button
+    int chooseNameY = 150; //Set y position of choose name button
     boolean overCustomize = false; //Set variable indicating mouse is over button to false
-    int customizeX = 0; //Set x position of customize button
-    int customizeY = 0; //Set y position of customize button
+    int customizeX = 230; //Set x position of customize button
+    int customizeY = 260; //Set y position of customize button
     boolean overStart = false; //Set variable indicating mouse is over button to false
-    int startX = 0; //Set x position of start button
-    int startY = 0; //Set y position of start button
+    int startX = 230; //Set x position of start button
+    int startY = 370; //Set y position of start button
     
     //Buttons for Stage -3 (Customize Clothes)
     boolean overLeftArrow = false; //Set variable indicating mouse is over left arrow button to false
-    int leftArrowX = 0; //Set x position of left arrow button
-    int leftArrowY = 0; //Set y position of left arrow button
+    int leftArrowX = 70; //Set x position of left arrow button
+    int leftArrowY = 245; //Set y position of left arrow button
     boolean overRightArrow = false; //Set variable indicating mouse is over right arrow button to false
-    int rightArrowX = 0; //Set x position of right arrow button
-    int rightArrowY = 0; //Set y position of right arrow button
+    int rightArrowX = 602; //Set x position of right arrow button
+    int rightArrowY = 245; //Set y position of right arrow button
             
      
     //Declare images
@@ -67,13 +72,21 @@ public class MySketch extends PApplet {
     PImage chooseNameButton;  //Custom name button
     PImage customizeButton;   //Custom clothes button
     PImage startButton;       //Start button
+    //Declare images for Stage -2 (Choose Name)
+    PImage chooseNameBG; //Choose name background
+    PImage nameErrorMessage; //Error message for entered name length
     //Declare images for Stage -3 (Custom Clothes)
+    PImage customizeClothesBG; //Customize clothes background
     PImage currentChoice;  //Current character choice (v1/v2/v3)
     PImage character1;     //Character clothes v1
     PImage character2;     //Character clothes v2
     PImage character3;     //Character clothes v3
     PImage leftButton;     //Left arrow button
     PImage rightButton;    //Right arrow button
+    //Declare images for Stage 1 (Begin Story)
+    PImage tellStory1;     //Tell the player about the story before they begin (part 1)
+    PImage tellStory2;     //Tell the player about the story before they begin (part 2)
+    PImage pressSpaceToContinue;  //Message telling the player to press space to continue
     
     
     
@@ -84,6 +97,7 @@ public class MySketch extends PApplet {
     public void setup() {
         background(100, 100, 100); ////Set background
         textSize(20); //Set text size
+        Player player; //Player object representing character
         
         //Load images
         //Load images for Stage 0 (Main Menu)
@@ -104,14 +118,24 @@ public class MySketch extends PApplet {
         characterSetup = loadImage("images/charactersetupbg.jpg");    //Character setup background
         chooseNameButton = loadImage("images/choosenamebutton.jpg");  //Custom name button
         customizeButton = loadImage("images/customizebutton.jpg");    //Custom clothes button
-        startButton = loadImage("images.startbutton.jpg");            //Start button
+        startButton = loadImage("images/startbutton.jpg");            //Start button
+        
+        //Load images for Stage -2 (Choose Name)
+        chooseNameBG = loadImage("images/choosenamebg.jpg");  //Choose name background
+        nameErrorMessage = loadImage("images/nameerrormessage.png");  //Error message for entered name length
         
         //Load images for Stage -3 (Custom Clothes)
-        character1 = loadImage("images.character1.jpg");    //Character clothes v1
-        character2 = loadImage("images.character2.jpg");    //Character clothes v2
-        character3 = loadImage("images.character3.jpg");    //Character clothes v3
-        leftButton = loadImage("images.leftbutton.jpg");    //Left arrow button
-        rightButton = loadImage("images.rightbutton.jpg");  //Right arrow button
+        customizeClothesBG = loadImage("images/customizeclothesbg.jpg");  //Customize clothes background
+        character1 = loadImage("images/character1.png");    //Character clothes v1
+        character2 = loadImage("images/character2.png");    //Character clothes v2
+        character3 = loadImage("images/character3.png");    //Character clothes v3
+        leftButton = loadImage("images/leftbutton.jpg");    //Left arrow button
+        rightButton = loadImage("images/rightbutton.jpg");  //Right arrow button
+        
+        //Load images for Stage 1 (Begin Story)
+        tellStory1 = loadImage("images/story1.jpg");    //Part 1 of story introduction
+        tellStory2 = loadImage("images/story2.jpg");    //Part 2 of story introduction
+        pressSpaceToContinue = loadImage("images/spacetocontinue.png");  //Press SPACE to continue message
         
     }
     
@@ -156,9 +180,29 @@ public class MySketch extends PApplet {
             }
             * */
             
+        //Choose Character's Name
+        } else if (stage == -2) {  
+            customName = true; //Set variable indicating that user entered their own name for the character to true
+    
+            //Set choose name background
+            background(chooseNameBG);
+            //Set fill colour to black for user input text
+            System.out.println("a");
+            fill(0);
+            //Display user input text
+            text(userInput, 450, 185);
             
+            if(nameError) ////Remember to set it back to false and put default name "don juan" as well as clothes
+                image(nameErrorMessage, 225, 200);
+            
+            
+        
+        //Customize Character's Appearance
         } else if (stage == -3) {
+            customCharacter = true; //Set variable indicating that user chose character's clothing to true
             
+            //Set customize clothes background
+            background(customizeClothesBG);
             
             //If user's choice is currently character 1, the image is shown on the screen
             if (chooseCharacter1)
@@ -178,13 +222,36 @@ public class MySketch extends PApplet {
             
         
         } else if (stage == 1) {
-            if (chooseCharacter1)
-                //Player player = new Player().... "character1";
-                System.out.println("a");
-            else if (chooseCharacter2)
-                System.out.println("b");
+            if (currentScreen1 == 1)
+                background(tellStory1);
+            else if (currentScreen1 == 2)
+                background(tellStory2);
             else
-                System.out.println("c");
+                background(selectTraits);
+            
+            image(pressSpaceToContinue, 280, 450);
+            
+            /**
+            if (customName && customCharacter) {
+                if (chooseCharacter1)
+                    player = new Player(this, 0, 0, enteredName, int characterTraits, Inventory inventory, String imagePat);
+                else if (chooseCharacter2)
+                    player = new Player(this, 0, 0, enteredName, int characterTraits, Inventory inventory, String imagePat);
+                else
+                    player = new Player(this, 0, 0, enteredName, int characterTraits, Inventory inventory, String imagePat);
+            } else if (customName) {
+                player = new Player(this, 0, 0, enteredName, int characterTraits, Inventory inventory);
+            } else if (customCharacter) {
+                if (chooseCharacter1)
+                   player = new Player(this, 0, 0, int characterTraits, Inventory inventory, String imagePat);
+               else if (chooseCharacter2)
+                   player = new Player(this, 0, 0, int characterTraits, Inventory inventory, String imagePat);
+               else
+                   player = new Player(this, 0, 0, int characterTraits, Inventory inventory, String imagePat);
+            } else {
+                player = new Player(this, 0, 0, int characterTraits, Inventory inventory);
+            }
+            * */
                 
         } 
     }
@@ -210,30 +277,35 @@ public class MySketch extends PApplet {
        
     
     
-        //Set boolean variable of a button to true if the mouse if over it (used for button clicks)
-        //Mouse is over a specific button in Stage 0 (Main Menu)
-        if (overImage(startGame, startGameX, startGameY)) //Start game button
-            overStartGame = true; //Set variable indicating mouse is over start game button to true
-        else if (overImage(leaderboard, leaderboardX, leaderboardY)) //Show leaderboard button
-            overLeaderboard = true; //Set variable indicating mouse is over leaderboard button to true
-        else if (overImage(exitGame, exitGameX, exitGameY)) //Exit game button
-            overExitGame = true; //Set variable indicating mouse is over exit gamebutton to true
-        
-        //Mouse is over a specific button in Stage 1 (Character Setup)
-        else if (overImage(chooseNameButton, chooseNameX, chooseNameY)) //Choose name button
-            overChooseName = true; //Set variable indicating mouse is over choose name button to true
-        else if (overImage(customizeButton, customizeX, customizeY)) //Customize clothes button
-            overCustomize = true; //Set variable indicating mouse is over customize clothes button to true
-        else if (overImage(startButton, startX, startY)) //Start button
-            overStart = true; //Set variable indicating mouse is over start button to true
-        
-        //Mouse is over a specific button in Stage -3 (Customize Clothes)
-        else if (overImage(leftButton, leftArrowX, leftArrowY)) //Left arrow button
-            overLeftArrow = true; //Set variable indicating mouse is over left arrow button to true
-        else if (overImage(rightButton, rightArrowX, rightArrowY)) //Right arrow button
-            overRightArrow = true; //Set variable indicating mouse is over right arrow button to true
-        
+        //Stage 0 (Main Menu)
+        if (stage == 0) {
+            //Set boolean variable of a button to true if the mouse if over it (used for button clicks)
+            //Mouse is over a specific button in Stage 0 (Main Menu)
+            if (overImage(startGame, startGameX, startGameY)) //Start game button
+                overStartGame = true; //Set variable indicating mouse is over start game button to true
+            else if (overImage(leaderboard, leaderboardX, leaderboardY)) //Show leaderboard button
+                overLeaderboard = true; //Set variable indicating mouse is over leaderboard button to true
+            else if (overImage(exitGame, exitGameX, exitGameY)) //Exit game button
+                overExitGame = true; //Set variable indicating mouse is over exit gamebutton to true
+        //Stage -1 (Character Setup)
+        } else if (stage == -1) {
+            //Mouse is over a specific button in Stage 1 (Character Setup)
+            if (overImage(chooseNameButton, chooseNameX, chooseNameY)) //Choose name button
+                overChooseName = true; //Set variable indicating mouse is over choose name button to true
+            else if (overImage(customizeButton, customizeX, customizeY)) //Customize clothes button
+                overCustomize = true; //Set variable indicating mouse is over customize clothes button to true
+            else if (overImage(startButton, startX, startY)) //Start button
+                overStart = true; //Set variable indicating mouse is over start button to true 
+        //Stage -3 (Customize Clothes)
+        } else if (stage == -3) {
+            //Mouse is over a specific button in Stage -3 (Customize Clothes)
+            if (overImage(leftButton, leftArrowX, leftArrowY)) //Left arrow button
+                overLeftArrow = true; //Set variable indicating mouse is over left arrow button to true
+            else if (overImage(rightButton, rightArrowX, rightArrowY)) //Right arrow button
+                overRightArrow = true; //Set variable indicating mouse is over right arrow button to true
+        }
        
+        
     
         
         //If mouse is hovering over the the 
@@ -278,9 +350,12 @@ public class MySketch extends PApplet {
             else if (overCustomize) 
                 stage = -3; //Go to customize character screen
             //If mouse is over start button when mouse is clicked
-            else if (overStart) 
+            else if (overStart) {
                 stage = 1; //Begin story
+            }
             
+    
+    
         //Stage -3 (Customize Clothes)
         } else if (stage == -3) {
             //If mouse is over left arrow button when mouse is clicked
@@ -311,8 +386,44 @@ public class MySketch extends PApplet {
         
     
     }
-    
-    
+
+      
+    @Override
+    public void keyPressed() {
+        if (key == ' ') {
+            if (stage == -3)
+                stage = -1;
+            //Spacebar is pressed in Stage 1 (Begin Story)
+            else if (stage == 1) {
+                if (currentScreen1 == 1) //If screen currently being shown is the first part of the story introduction
+                    currentScreen1 = 2;
+                else if (currentScreen1 == 2) //If screen currently being shown is the second part of the story introduction
+                    currentScreen1 = 3;
+                else {
+                    stage = 2;
+                    currentScreen1 = 1; //Reset screen to be shown for Stage 1 to the first part of the story introduction
+                } 
+                    
+            }
+        }
+        
+        if (stage == -2) {
+            if (keyCode == ENTER) { //If enter key is pressed
+                nameError = false; //Set error indicator for name to false in case it was set to true previously
+                if (userInput.isEmpty() || userInput.length() > 10)
+                    nameError = true;
+                else            
+                    enteredName = userInput;
+            } else if (keyCode == BACKSPACE) {
+                if (userInput.length() > 0) 
+                    userInput = userInput.substring(0, userInput.length() - 1); //Remove last character from user input string
+             
+            } else if (key != CODED) { // checks if it is an ASCII character
+                userInput += key; // add each keystroke to user input string
+            } // end nested if
+        }
+        
+    }
      
    boolean overImage(PImage image, int x, int y)  {
         if (mouseX >= x && mouseX <= x+image.width && 
